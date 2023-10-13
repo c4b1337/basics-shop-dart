5456afe389581b16b36e3e350174eef9426ba6c7
user <user@users-MacBook-Pro.local>
2023-10-13 17:25:10 +0300

    -add search

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,6 +10,39 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Shop Page'));
+    return Column(
+      children: [
+        //search bar
+        Container(
+          padding: const EdgeInsets.all(13.0),
+          margin: const EdgeInsets.all(13.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search Bar',
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey[600],
+                size: 27,
+              ),
+            ],
+          ),
+        ),
+        //message
+
+        //hot picks
+      ],
+    );
   }
 }
